303b6a4c075538c67a949013f7faf44253952c57
Bill Maxwell <cloudnautique@users.noreply.github.com>
2023-07-27 18:05:20 -0700

    updated to use external id
    Signed-off-by: Bill Maxwell <cloudnautique@users.noreply.github.com>

--- rds/go.mod
+++ rds/go.mod
@@ -4,9 +4,9 @@ go 1.20
 
 require (
 	github.com/acorn-io/services/aws/libs/common v0.0.0
-	github.com/aws/aws-cdk-go/awscdk/v2 v2.87.0
+	github.com/aws/aws-cdk-go/awscdk/v2 v2.88.0
 	github.com/aws/constructs-go/constructs/v10 v10.2.69
-	github.com/aws/jsii-runtime-go v1.84.0
+	github.com/aws/jsii-runtime-go v1.85.0
 	github.com/sirupsen/logrus v1.9.3
 )
 
@@ -14,13 +14,15 @@ replace github.com/acorn-io/services/aws/libs/common v0.0.0 => ../libs/common
 
 require (
 	github.com/Masterminds/semver/v3 v3.2.1 // indirect
-	github.com/cdklabs/awscdk-asset-awscli-go/awscliv1/v2 v2.2.198 // indirect
+	github.com/cdklabs/awscdk-asset-awscli-go/awscliv1/v2 v2.2.200 // indirect
 	github.com/cdklabs/awscdk-asset-kubectl-go/kubectlv20/v2 v2.1.2 // indirect
-	github.com/cdklabs/awscdk-asset-node-proxy-agent-go/nodeproxyagentv5/v2 v2.0.165 // indirect
+	github.com/cdklabs/awscdk-asset-node-proxy-agent-go/nodeproxyagentv5/v2 v2.0.166 // indirect
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/yuin/goldmark v1.4.13 // indirect
+	github.com/yuin/goldmark v1.5.5 // indirect
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
-	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/tools v0.9.3 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/tools v0.11.0 // indirect
 )
--- rds/scripts/apply.sh
+++ rds/scripts/apply.sh
@@ -1,14 +1,77 @@
 #!/bin/bash
-set -e -x
 
-STACK_NAME="acorn-${ACORN_ACCOUNT}-${ACORN_PROJECT}-${ACORN_NAME//\./-}-${DB_NAME//\./-}"
+STACK_NAME="${ACORN_EXTERNAL_ID}"
 
 # Start logging
 ./scripts/stacklog.sh ${STACK_NAME} &
+. ./scripts/record_event.sh
 
-if [ "${ACORN_EVENT}" = "delete" ]; then
+set -e -x
+
+cdk_synth() {
+    cat cdk.context.json
+    cdk synth --path-metadata false --lookups false > cfn.yaml
+    cat cfn.yaml
+}
+
+apply_and_render() {
+  # Run CDK synth
+  cdk_synth
+  
+  # Run CloudFormation
+  aws cloudformation deploy --template-file cfn.yaml --stack-name "${STACK_NAME}" --capabilities CAPABILITY_IAM --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --no-cli-pager
+  aws cloudformation describe-stacks --stack-name "${STACK_NAME}" --query 'Stacks[0].Outputs' > outputs.json
+
+  # Render Output
+  PORT="$(          jq -r '.[] | select(.OutputKey=="port")            |.OutputValue' outputs.json )"
+  ADDRESS="$(       jq -r '.[] | select(.OutputKey=="host")            |.OutputValue' outputs.json )"
+  ADMIN_USERNAME="$(jq -r '.[] | select(.OutputKey=="adminusername")   |.OutputValue' outputs.json )"
+  PASSWORD_ARN="$(  jq -r '.[] | select(.OutputKey=="adminpasswordarn")|.OutputValue' outputs.json )"
+
+  # Turn off echo
+  set +x
+  ADMIN_PASSWORD="$(aws --output json secretsmanager get-secret-value --secret-id "${PASSWORD_ARN}" --query 'SecretString' | jq -r .|jq -r .password)"
+  record_event "${ACORN_NAME}${ACORN_EVENT}d" "RDS Database ${ACORN_EVENT}d successfully"
+
+  cat > /run/secrets/output <<EOF
+services: rds: {
+  default: true
+  address: "${ADDRESS}"
+  ports: [${PORT}]
+  data: dbName: "${DB_NAME}"
+}
+
+secrets: "admin": {
+	type: "basic"
+	data: {
+    username: "${ADMIN_USERNAME}"
+    password: "${ADMIN_PASSWORD}"
+  }
+}
+EOF
+
+  if [ -z "${DB_USERNAME}" ]; then
+    echo 'services: rds: secrets: ["admin"]' >> /run/secrets/output
+  else
+    echo 'services: rds: secrets: ["admin", "user"]' >> /run/secrets/output
+  fi
+}
+
+delete_stack() {
+    apply_and_render
+    if $(grep 'DeletionProtection: true' cfn.yaml > /dev/null 2>&1); then
+        echo "DeletionProtection is enabled, update acorn app with '--deletion-protection=false' to delete this stack..."
+        exit 1
+    fi
+
+    # Run CloudFormation
     aws cloudformation delete-stack --stack-name "${STACK_NAME}"
     aws cloudformation wait stack-delete-complete --stack-name "${STACK_NAME}" --no-cli-pager
+    record_event "${ACORN_NAME}${ACORN_EVENT}d" "RDS Database ${ACORN_EVENT}d successfully"
+}
+
+if [ "${ACORN_EVENT}" = "delete" ]; then
+    delete_stack
     exit 0
 fi
 
@@ -16,48 +79,9 @@ fi
 STATUS=$(aws cloudformation describe-stacks --stack-name "${STACK_NAME}" | jq -r '.Stacks[0].StackStatus')
 
 if [ "$STATUS" = "DELETE_FAILED" ]; then
-    aws cloudformation delete-stack --stack-name "${STACK_NAME}"
-    aws cloudformation wait stack-delete-complete --stack-name "${STACK_NAME}" --no-cli-pager
+    delete_stack
 fi
-
-# Run CDK synth
-cat cdk.context.json
-cdk synth --path-metadata false --lookups false > cfn.yaml
-cat cfn.yaml
 
 # Run CloudFormation
-aws cloudformation deploy --template-file cfn.yaml --stack-name "${STACK_NAME}" --capabilities CAPABILITY_IAM --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --no-cli-pager
-aws cloudformation describe-stacks --stack-name "${STACK_NAME}" --query 'Stacks[0].Outputs' > outputs.json
-
-# Render Output
-PORT="$(          jq -r '.[] | select(.OutputKey=="port")            |.OutputValue' outputs.json )"
-ADDRESS="$(       jq -r '.[] | select(.OutputKey=="host")            |.OutputValue' outputs.json )"
-ADMIN_USERNAME="$(jq -r '.[] | select(.OutputKey=="adminusername")   |.OutputValue' outputs.json )"
-PASSWORD_ARN="$(  jq -r '.[] | select(.OutputKey=="adminpasswordarn")|.OutputValue' outputs.json )"
-
-# Turn off echo
-set +x
-ADMIN_PASSWORD="$(aws --output json secretsmanager get-secret-value --secret-id "${PASSWORD_ARN}" --query 'SecretString' | jq -r .|jq -r .password)"
-
-cat > /run/secrets/output <<EOF
-services: rds: {
-    default: true
-    address: "${ADDRESS}"
-    ports: [${PORT}]
-    data: dbName: "${DB_NAME}"
-}
-
-secrets: "admin": {
-	type: "basic"
-	data: {
-        username: "${ADMIN_USERNAME}"
-        password: "${ADMIN_PASSWORD}"
-    }
-}
-EOF
-
-if [ -z "$MYSQL_USER" ]; then
-  echo 'services: rds: secrets: ["admin"]' >> /run/secrets/output
-else
-  echo 'services: rds: secrets: ["admin", "user"]' >> /run/secrets/output
-fi
+apply_and_render
+exit 0
