b44d43ca9f0c5630b797be40cba67fa38879e9ec
Kyle McGough <kylemcgough@hey.com>
2023-08-21 13:59:48 -0700

    use aws-sdk-2, switch to admin service as default for testing

--- s3/examples/plaintext/main.go
+++ s3/examples/plaintext/main.go
@@ -2,42 +2,48 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"io"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func main() {
-	// Create a session that gets credential values from ~/.aws/credentials
-	// and the default region from ~/.aws/config
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	svc := s3.New(sess)
+	client := s3.NewFromConfig(cfg)
 
 	bucket := aws.String(os.Getenv("BUCKET_NAME"))
 
 	http.HandleFunc("/get", func(response http.ResponseWriter, request *http.Request) {
 		key := aws.String(request.URL.Query().Get("key"))
 
-		output, err := svc.GetObject(&s3.GetObjectInput{
+		output, err := client.GetObject(context.Background(), &s3.GetObjectInput{
 			Key:    key,
 			Bucket: bucket,
 		})
-
 		if err != nil {
 			log.Printf("failed to get object: %v", err)
 			response.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
-		response.Write([]byte(output.String()))
+		value, err := io.ReadAll(output.Body)
+		if err != nil {
+			log.Printf("failed to read value body: %v", err)
+			response.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		response.Write(value)
 	})
 
 	http.HandleFunc("/put", func(response http.ResponseWriter, request *http.Request) {
@@ -50,7 +56,7 @@ func main() {
 			return
 		}
 
-		_, err = svc.PutObject(&s3.PutObjectInput{Key: key, Bucket: bucket, Body: bytes.NewReader(body)})
+		_, err = client.PutObject(context.Background(), &s3.PutObjectInput{Key: key, Bucket: bucket, Body: bytes.NewReader(body)})
 		if err != nil {
 			log.Printf("failed to put object: %v", err)
 			response.WriteHeader(http.StatusInternalServerError)
